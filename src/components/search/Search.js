cfa6b70f0d17449b44b2e644946862731824981d
Prachee-Personal <pracheesingh07@gmail.com>
2021-03-05 10:09:09 +0530

    changes in ui

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,78 +1,83 @@
-import React,{Component,useState} from 'react';
+import React, { Component, useState } from 'react';
 import axios from 'axios';
 import ImageResult from "./ImageResult/ImageResult";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css'
 
-const [currentPage, setCurrentPage] = useState(0);
-class Search extends Component{
-    state={
-        searchText:'',
-        apiUrl:'https://pixabay.com/api',
-        apiKey:'17241914-90da7b93c0ccceb734849dcd1',
-        images:[]
+class Search extends Component {
+    state = {
+        searchText: '',
+        apiUrl: 'https://pixabay.com/api',
+        apiKey: '17241914-90da7b93c0ccceb734849dcd1',
+        images: []
     };
 
     imagesArray = [];
     componentDidMount() {
         console.log("I am called")
-        this.callInitialImageAPI('WildCat').then( response => {
+        this.callInitialImageAPI('mountains').then(response => {
             console.log("The response is: " + response)
-            this.setState({images: this.imagesArray}); 
-            console.log("the result is  :: ",this.state.images)     
+            this.setState({ images: this.imagesArray });
+            console.log("the result is  :: ", this.state.images)
         });
     }
 
     callInitialImageAPI(imageSearchText) {
         return new Promise((resolve, reject) => {
             axios.get(
-                `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
-                    imageSearchText
+                `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${imageSearchText
                 }&image_type=photo&safesearch=true`
             )
-            .then(res=>{
-                console.log(res.data.hits)
-                // this.imagesArray.concat(res.data.hits)
-                this.imagesArray = [...this.imagesArray, ...res.data.hits]
-                resolve(this.imagesArray)
-            })
-            .catch(err=>{
-                console.log(err);
-                reject(err);
-            });
+                .then(res => {
+                    console.log(res.data.hits)
+                    // this.imagesArray.concat(res.data.hits)
+                    this.imagesArray = [...this.imagesArray, ...res.data.hits]
+                    resolve(this.imagesArray)
+                })
+                .catch(err => {
+                    console.log(err);
+                    reject(err);
+                });
         });
     }
 
-    onTextChange=(e)=>{
-        const val=e.target.value;
-        this.setState({[e.target.name]:val},()=>{
-            if(val==='')
-            {
-                this.setState({images:this.imagesArray});
+    onTextChange = (e) => {
+        const val = e.target.value;
+        this.setState({ [e.target.name]: val }, () => {
+            if (val === '') {
+                this.setState({ images: this.imagesArray });
             }
-            else{
-            axios
-            .get(
-                `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
-                    this.state.searchText
-                }&image_type=photo&safesearch=true`
-            )
-            .then(res=>this.setState({images:res.data.hits}))
-            .catch(err=>console.log(err));
+            else {
+                axios
+                    .get(
+                        `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText
+                        }&image_type=photo&safesearch=true`
+                    )
+                    .then(res => this.setState({ images: res.data.hits }))
+                    .catch(err => console.log(err));
             }
         });
     };
-    render(){
+    render() {
         console.log(this.state.images);
-        return(
-            <center className="searchBox">
-            <input type="text" 
-        placeholder="Search for images"
-        name="searchText"
-        value={this.state.searchText}
-        onChange={this.onTextChange}
-             />
-<br />
-{this.state.images.length>0?(<ImageResult images={this.state.images}/>):null}
-            </center>
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col md-12">
+                        <h1 className="title">PhotoSearch</h1>
+                        <center>
+                            <input className="searchbox" type="text"
+                                placeholder="Search For Images"
+                                name="searchText"
+                                value={this.state.searchText}
+                                onChange={this.onTextChange}
+                            />
+                            <br />
+                            {this.state.images.length > 0 ? (<ImageResult images={this.state.images} />) : null}
+                        </center>
+                    </div>
+                </div>
+            </div>
 
         )
     }
